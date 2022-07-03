514d984ae4144f203e2416a2f70252d068a15b9c
thevostle <technic.dima@gmail.com>
2022-07-03 22:05:14 +0300

    Update scene.js

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -1,15 +1,15 @@
 class Scene {
 	constructor(data) {
-        this.data = data;
+		this.data = data;
 		this.init();
 	}
 
 	init() {
 		let scene = new PIXI.Stage();
 
-        // заполняем сцену новыми объектами
-        for (i in data) {
-            // спаун объекта нужного типа с конкретными параметрами
-        }
+		// заполняем сцену новыми объектами
+		for (i in data) {
+			// спаун объекта нужного типа с конкретными параметрами
+		}
 	}
 }
